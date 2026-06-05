68837ed312f4d50c4c9da82de0365367441cba72
AndentEnya <andent.music@gmail.com>
2026-06-05 22:11:01 +0500

    Add service worker: auto cache + update banner on new version

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'nutribase-v6';
+const ASSETS = [
+  '/nutribase_kz/',
+  '/nutribase_kz/index.html',
+  '/nutribase_kz/style.css',
+  '/nutribase_kz/app.js',
+  '/nutribase_kz/auth.js',
+  '/nutribase_kz/firebase-config.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = e.request.url;
+  // Не кэшируем Firebase/Google запросы
+  if (url.includes('firebase') || url.includes('googleapis') || url.includes('gstatic')) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
